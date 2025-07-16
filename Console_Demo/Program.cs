6d9488562e82b8812c39de9a5b789839bae76f2b
RohitYewale880 <rohityewale2020@gmail.com>
2025-07-16 17:41:51 +0530

    operaters

--- Console_Demo/Program.cs
+++ Console_Demo/Program.cs
@@ -10,25 +10,71 @@ namespace Console_Demo
     {
         static void Main(string[] args)
         {
-            // Printing the statements..
+            // Printing the statements =>
+
             //Console.Write("Enter The word you want to display : ");
             //string inp = Console.ReadLine();
             //Console.WriteLine(inp);
             //Console.ReadLine();    
 
-            // Addition of two numbers
+
+            // Addition of two numbers =>
+
             //int a = 12;            
             //int b = 12;            
             //Console.Write("Addition of two number is => ");
             //Console.WriteLine(a + b);
             //Console.ReadLine();    
 
-            // print your name user gives the name what he wants to prints
 
-            Console.WriteLine("Enter Your name =>");
-            string name = Console.ReadLine();
-            Console.WriteLine("My name is " + name + ".");
-            Console.Read();
-        }                          
+            // print your name user gives the name what he wants to prints =>
+
+            //Console.WriteLine("Enter Your name =>");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("My name is " + name + ".");
+            //Console.Read();
+
+
+            //Operators in C# =>
+
+            // Arthmetic operaters =
+            //int a = 4, b = 2;
+            //Console.WriteLine("Calculation result of 4 + 2 is = " + (a + b));
+            //Console.WriteLine("Calculation result of 4 - 2 is = " + (a - b));
+            //Console.WriteLine("Calculation result of 4 * 2 is = " + (a * b));
+            //Console.WriteLine("Calculation result of 4 / 2 is = " + (a / b));
+            //Console.WriteLine("Calculation result of 4 % 2 is = " + (a % b));
+            //Console.ReadLine();
+
+
+            //Assignment Operater
+            //int a = 4, b = a;
+            //Console.WriteLine(( b +=4 ));
+            //Console.WriteLine(( b -= 4));
+            //Console.WriteLine(( b *= 4));
+            //Console.WriteLine(( b /= 4));
+            //Console.WriteLine(( b %= 4));
+            //Console.ReadLine();
+
+            //Comparision Operater
+            //int a = 4, b = 2;
+            //Console.WriteLine(a == b);
+            //Console.WriteLine(a != b);
+            //Console.WriteLine(a > b);
+            //Console.WriteLine(a < b);
+            //Console.WriteLine(a >= b);
+            //Console.WriteLine(a <= b);
+            //Console.ReadLine();
+
+            //logical Operater
+            Console.WriteLine(true && true);
+            Console.WriteLine(true && false);
+            Console.WriteLine(false && false);
+
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+            Console.ReadLine();
+        }
     }
 }
