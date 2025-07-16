edf96b607756b38852a2393e090e7d1b770538d6
RohitYewale880 <rohityewale2020@gmail.com>
2025-07-16 16:37:03 +0530

    print name

--- Console_Demo/Program.cs
+++ Console_Demo/Program.cs
@@ -10,16 +10,25 @@ namespace Console_Demo
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter The word you want to display : ");
-            string inp = Console.ReadLine();
-            Console.WriteLine(inp);
-            Console.ReadLine();
+            // Printing the statements..
+            //Console.Write("Enter The word you want to display : ");
+            //string inp = Console.ReadLine();
+            //Console.WriteLine(inp);
+            //Console.ReadLine();    
 
-            int a = 12;
-            int b = 12;
-            Console.Write("Addition of two number is => ");
-            Console.WriteLine(a + b);
-            Console.ReadLine();
-        }
+            // Addition of two numbers
+            //int a = 12;            
+            //int b = 12;            
+            //Console.Write("Addition of two number is => ");
+            //Console.WriteLine(a + b);
+            //Console.ReadLine();    
+
+            // print your name user gives the name what he wants to prints
+
+            Console.WriteLine("Enter Your name =>");
+            string name = Console.ReadLine();
+            Console.WriteLine("My name is " + name + ".");
+            Console.Read();
+        }                          
     }
 }
