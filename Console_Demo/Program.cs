371a7e1bf288af712177d3f99d2286be7f281de9
RohitYewale880 <rohityewale2020@gmail.com>
2025-07-21 16:14:18 +0530

    string in C#

--- Console_Demo/Program.cs
+++ Console_Demo/Program.cs
@@ -67,13 +67,52 @@ namespace Console_Demo
             //Console.ReadLine();
 
             //logical Operater
-            Console.WriteLine(true && true);
-            Console.WriteLine(true && false);
-            Console.WriteLine(false && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(false && false);
 
-            Console.WriteLine(true || true);
-            Console.WriteLine(true || false);
-            Console.WriteLine(false || false);
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+            //Console.ReadLine();
+
+
+
+            // Math classes in the C# =>
+
+            //Console.WriteLine(Math.Abs(-5));
+            //Console.WriteLine(Math.Ceiling(4.5));
+            //Console.WriteLine(Math.Floor(4.8));
+            //Console.WriteLine(Math.Round(4.5));
+            //Console.WriteLine(Math.Max(10,20));
+            //Console.WriteLine(Math.Min(10,20));
+            //Console.WriteLine(Math.Sqrt(25));
+            //Console.WriteLine(Math.Pow(2,3));
+            //Console.WriteLine(Math.Sin(Math.PI/2));
+            //Console.WriteLine(Math.Cos(0));
+            //Console.WriteLine(Math.Tan(0));
+            //Console.WriteLine(Math.Log(Math.E));
+            //Console.WriteLine(Math.Log10(100));
+            //Console.WriteLine(Math.Truncate(3.75));
+            ////Maths Constants in the C#
+            //Console.WriteLine(Math.PI);
+            //Console.WriteLine(Math.E);
+            //Console.ReadLine();
+
+
+            //String in C# =>
+
+            string name = " Rohit Yewale";
+            Console.WriteLine(name);
+            Console.WriteLine(name.Length);
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(name.Trim());
+            Console.WriteLine(name.Substring(3,3));
+            Console.WriteLine(name.Replace("R","M"));
+            Console.WriteLine(name.Contains("hit"));
+            Console.WriteLine(name.IndexOf("t"));
+            Console.WriteLine(name.Split(' '));
             Console.ReadLine();
         }
     }
